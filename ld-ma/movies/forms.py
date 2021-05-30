586acb389f94b5ba671f5cf6e415a8f76cd6d92a
jay <2018ecjay013@ldce.ac.in>
2021-05-30 18:10:53 +0530

    hjjfv

--- ld-ma/movies/forms.py
+++ ld-ma/movies/forms.py
@@ -7,7 +7,7 @@ from .models import User,UserProfile
 class SignUpForm(forms.ModelForm):
         username = forms.CharField(max_length=30, required=False, help_text='Optional.')
         email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-        model = User
+        model = UserProfile
         class Meta:
                 fields = ['username','password','password1','email']
         
