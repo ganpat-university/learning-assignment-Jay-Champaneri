db685ef39ee6451c83065f17cc4c56a286a929f7
jay <2018ecjay013@ldce.ac.in>
2021-05-30 18:08:48 +0530

    hgf

--- ld-ma/movies/forms.py
+++ ld-ma/movies/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User,UserProfile
         
         
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.ModelForm):
         username = forms.CharField(max_length=30, required=False, help_text='Optional.')
         email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
         model = User
